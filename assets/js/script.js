59a07666a570971d065d11a5bd049bd798f52cc4
Anthony Catan-Cavery <a.catancavery@gmail.com>
2025-11-26 19:07:40 +0100

    structure de base

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Structure de Base
+
+
+// Récupération du canvas et du contexte
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+// Test : affiche dans la console pour vérifier que tout est lié
+console.log('Canvas !', canvas.width, canvas.height);
